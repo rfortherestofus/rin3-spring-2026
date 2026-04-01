1374aff2412d1c1cb8ba8ca390df45fdf99f1f51
David Keyes <david@rfortherestofus.com>
2026-04-01 15:44:52 -0700

    Update week 4 slides and misc files for spring 2026 cohort

--- misc/geomtextpath.R
+++ misc/geomtextpath.R
@@ -2,18 +2,27 @@ library(tidyverse)
 library(geomtextpath)
 
 # Load data
-penguins <- read_csv("https://raw.githubusercontent.com/rfortherestofus/rin3-fall-2023/main/data-raw/penguins.csv")
+penguins <- read_csv(
+  "https://raw.githubusercontent.com/rfortherestofus/rin3-spring-2026/main/data-raw/penguins.csv"
+)
 
 # Create a scatter plot and add geomtextpath to show a smooth best fit line with species name
-ggplot(data = penguins, aes(x = bill_length_mm,
-                            y = flipper_length_mm,
-                            group = species,
-                            color = species)) +
+ggplot(
+  data = penguins,
+  aes(
+    x = bill_length_mm,
+    y = flipper_length_mm,
+    group = species,
+    color = species
+  )
+) +
   geom_point() +
-  geomtextpath::geom_labelpath(aes(label = paste0(species)),
-                               stat = "smooth",
-                               alpha = 0.8) +
-  theme(legend.position="none")
+  geomtextpath::geom_labelpath(
+    aes(label = paste0(species)),
+    stat = "smooth",
+    alpha = 0.8
+  ) +
+  theme(legend.position = "none")
 
 
 # Gapminder ---------------------------------------------------------------
@@ -23,9 +32,7 @@ library(gapminder)
 
 gapminder |>
   filter(continent == "Asia") |>
-  ggplot(aes(x = year,
-             y = lifeExp,
-             label = country)) +
+  ggplot(aes(x = year, y = lifeExp, label = country)) +
   geom_textline() +
   # geom_line() +
   facet_wrap(~country)
