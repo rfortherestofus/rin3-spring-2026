307af97e1c04768c6f748fc0631f49f11d29f56c
David Keyes <david@rfortherestofus.com>
2026-05-07 09:46:38 -0700

    week 9

--- misc/cbem-live-coding.R
+++ misc/cbem-live-coding.R
@@ -1,9 +1,6 @@
 #  Recreating this: https://show.rfor.us/cditiy
 
 library(tidyverse)
-library(ggchicklet)
-library(patchwork)
-library(scales)
 
 cbem <-
   read_csv("https://rin3spring2026.rfortherestofus.com/data-raw/cbem.csv")
